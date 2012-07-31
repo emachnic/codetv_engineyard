790ba6695b0919131df41db5531aeabd49c3ea40
Evan Machnic <emachnic@broadmac.net>
2012-07-31 09:59:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodetvEngineyard::Application.config.secret_token = '7e4a433405f715a3f58cf4dcb4432bb6854ce50dd4356478cf2597fb941367e64011e7908c4d8eb174f2bf4138df09568b9bdae728aa400f0f10c03fea9cb3da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CodetvEngineyard::Application.config.session_store :cookie_store, key: '_codetv_engineyard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CodetvEngineyard::Application.config.session_store :active_record_store
